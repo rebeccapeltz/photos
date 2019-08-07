fb9f0c094fe1a218680f7a82177fde153e78329a
rebeccapeltz <rebeccapeltz@gmail.com>
2019-08-06 17:06:21 -0700

    add select for selfie/world

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // Put variables in global scope to make them available to the browser console.
 const video = document.querySelector('video');
 const canvas = window.canvas = document.querySelector('canvas');
+const videoSelect = document.querySelector('#video-select')
+let localMediaStream = null;
 let fileData;
 canvas.width = 480;
 canvas.height = 360;
@@ -29,6 +31,11 @@ snapshotButton.addEventListener("click", event => {
   canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
   fileData = canvas.toDataURL('image/jpeg');
 })
+const stopStreamButton = document.querySelector('#stop-stream');
+stopStreamButton.addEventListener("click",event=>{
+  video.pause()
+  // localMediaStream.stop()
+})
 document.addEventListener("submit", event => {
   event.preventDefault()
   let cloudName = document.querySelector('input[name=cloud-name]').value;
@@ -48,12 +55,31 @@ const constraints = {
 };
 
 function handleSuccess(stream) {
+  localMediaStream = stream
   window.stream = stream; // make stream available to browser console
   video.srcObject = stream;
+  navigator.mediaDevices.enumerateDevices().then(deviceInfos=>{
+    window.deviceInfos = deviceInfos; // make available to console
+    console.log('Available input and output devices:', deviceInfos);
+    for (const deviceInfo of deviceInfos) {
+      const option = document.createElement('option');
+      option.value = deviceInfo.deviceId;
+      if (deviceInfo.kind === 'videoinput') {
+        option.text = deviceInfo.label || `Camera ${videoSelect.length + 1}`;
+        videoSelect.appendChild(option);
+      }
+    }
+  })
 }
 
 function handleError(error) {
   console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
 }
 
-navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+navigator.mediaDevices.getUserMedia(constraints).then(stream=>{
+  handleSuccess(stream)
+}).catch(error =>{
+  handleError(error)
+});
+
+  
